9f974b09fbec6cf49827ee8b46313f2a516dc1d1
Mark Claydon <mark@yallacooperative.com>
2023-07-11 00:34:36 +0100

    Added db test

--- web/db-test.js
+++ web/db-test.js
@@ -12,9 +12,44 @@ export const DB = {
   db: null,
   ready: null,
 
+  drop: async function () {
+    /* Drops the database */
+    await this.ready;
+    const query = `
+      DROP TABLE IF EXISTS ${this.shopifySessionsTable};
+    `;
+    await this.__query(query);
+  },
+
+  destroy: async function () {
+    /* Destroys the connection to the database */
+    await this.ready;
+    this.db.close();
+    this.db = null;
+    this.ready = null;
+  },
+
   init: async function () {
     /* Initializes the connection to the database */
-    this.db = this.db ?? new sqlite3.Database(DEFAULT_DB_FILE);
+    this.db = new sqlite3.Database(DEFAULT_DB_FILE);
+
+    const dropTableQuery = `
+    DROP TABLE IF EXISTS ${this.shopifySessionsTable};
+  `;
+    await this.__query(dropTableQuery);
+    const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS ${this.shopifySessionsTable} (
+      id TEXT PRIMARY KEY,
+      shop TEXT NOT NULL,
+      state INTEGER NOT NULL,
+      isOnline INTEGER NOT NULL,
+      accessToken TEXT NOT NULL,
+      onlineAccessInfo TEXT,
+      scope TEXT,
+      expires INTEGER
+    );
+  `;
+    await this.__query(createTableQuery);
     this.ready = Promise.resolve();
   },
   read: async function (id) {
