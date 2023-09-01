395cd9437174e18cd7a6ab34773923972d99c209
Hassan ElNajjar <hassan@upet.co>
2023-09-01 13:55:32 +0300

    check app deployment

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -1,17 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
-import { NavigationMenu } from "@shopify/app-bridge-react";
-import Routes from "./Routes";
+import { BrowserRouter } from 'react-router-dom';
+import { NavigationMenu } from '@shopify/app-bridge-react';
+import Routes from './Routes';
 
 import {
   AppBridgeProvider,
   QueryProvider,
-  PolarisProvider,
-} from "./components";
+  PolarisProvider
+} from './components';
 
 export default function App() {
   // Any .tsx or .jsx files in /pages will become a route
   // See documentation for <Routes /> for more info
-  const pages = import.meta.globEager("./pages/**/!(*.test.[jt]sx)*.([jt]sx)");
+
+  console.log('running app.jsx');
+  console.log('process.env', process.env);
+
+  const pages = import.meta.globEager('./pages/**/!(*.test.[jt]sx)*.([jt]sx)');
 
   return (
     <PolarisProvider>
@@ -21,9 +25,9 @@ export default function App() {
             <NavigationMenu
               navigationLinks={[
                 {
-                  label: "Page name",
-                  destination: "/pagename",
-                },
+                  label: 'Page name',
+                  destination: '/pagename'
+                }
               ]}
             />
             <Routes pages={pages} />
