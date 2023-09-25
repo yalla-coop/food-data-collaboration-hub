3efd1ed5d5bb13dbeb2704c5cd3cbf4a34fe340c
Hassan ElNajjar <hassan@upet.co>
2023-09-25 14:36:02 +0300

    solve un-resovled file

--- web/frontend/components/index.js
+++ web/frontend/components/index.js
@@ -1,5 +1,4 @@
 export { ProductsCard } from './ProductsCard';
-export { ProductsList } from './ProductsList';
 export { VariantCard } from './VariantCard';
 export { DatePickerComponent } from './DatePicker';
 export * from './providers';
