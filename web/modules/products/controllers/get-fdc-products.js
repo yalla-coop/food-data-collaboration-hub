0d745f862c80ec9e91587ee5b19a4f9efa15d919
Simon Dupree <simon@yallacooperative.com>
2024-01-22 19:25:55 +0100

    add connector helper functions

--- web/modules/products/controllers/get-fdc-products.js
+++ web/modules/products/controllers/get-fdc-products.js
@@ -2,9 +2,7 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 import { join } from 'path';
 
-import { Connector } from '@datafoodconsortium/connector';
-
-const connector = new Connector();
+import { connector } from '../../../connector/index.js';
 
 dotenv.config({
   path: join(process.cwd(), '.env')
@@ -32,13 +30,15 @@ const getFDCProducts = async (req, res, next) => {
         }
       }
     );
-    const dfcTestProductExports = data?.dfcTestProductExports;
-    const dfcTestProductImports = await connector.import(dfcTestProductExports);
-    console.log('dfcTestProductImports :>> ', dfcTestProductImports);
+
+    if (data?.exportedDFCProducts) {
+      const imports = await connector.import(data.exportedDFCProducts);
+
+      console.log('dfcTestProductImports :>> ', imports);
+    }
 
     return res.json(data);
   } catch (err) {
-    console.log('err', err);
     return next(err);
   }
 };
