5c6f6eaba3d2a678df73eccf6717a171a16b9e51
Simon Dupree <simon@yallacooperative.com>
2024-01-31 19:01:07 +0100

    rmv local configs

--- web/index.js
+++ web/index.js
@@ -1,7 +1,6 @@
 import app from './app.js';
 
-// const port = process.env.BACKEND_PORT || process.env.PORT || '3000';
-const port = 58939;
+const port = process.env.BACKEND_PORT || process.env.PORT || '3000';
 
 const PORT = parseInt(port, 10);
 
--- web/middleware/isAuthenticated.js
+++ web/middleware/isAuthenticated.js
@@ -6,48 +6,36 @@ const issuerURL = process.env.OIDC_ISSUER;
 
 const isAuthenticated = async (req, res, next) => {
   try {
-    req.user = {
-      id: 'e5b41da4-2633-412f-8210-1569a2d0ce20',
-      username: 'mark@yallacooperative.com',
-      name: 'Mark Claydon',
-      email: 'mark@yallacooperative.com',
-      accessToken:
-        'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJKVjg1bVRtUmh1MGtSeGNNb0FGUFl5azJMbS1WTExYV25HOG9HbUxNUkowIn0.eyJleHAiOjE3MDY3MjQ5MDMsImlhdCI6MTcwNjcyMzEwMywiYXV0aF90aW1lIjoxNzA1MzQwOTQxLCJqdGkiOiJiYzVkY2IxMi1jNDYxLTRhZDUtYmMyNy1lM2Q3MzQ5NzFiMDYiLCJpc3MiOiJodHRwczovL2xvZ2luLmxlc2NvbW11bnMub3JnL2F1dGgvcmVhbG1zL2RhdGEtZm9vZC1jb25zb3J0aXVtIiwiYXVkIjoiYWNjb3VudCIsInN1YiI6ImU1YjQxZGE0LTI2MzMtNDEyZi04MjEwLTE1NjlhMmQwY2UyMCIsInR5cCI6IkJlYXJlciIsImF6cCI6InlhbGxhZGV2Iiwic2Vzc2lvbl9zdGF0ZSI6ImQ0YTE2Nzk2LTI1ODgtNDk2NS04MjVjLTAxZTk0NDUxMzBkZCIsImFjciI6IjAiLCJyZWFsbV9hY2Nlc3MiOnsicm9sZXMiOlsiZGVmYXVsdC1yb2xlcy1kYXRhLWZvb2QtY29uc29ydGl1bSIsIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwic2lkIjoiZDRhMTY3OTYtMjU4OC00OTY1LTgyNWMtMDFlOTQ0NTEzMGRkIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJuYW1lIjoiTWFyayBDbGF5ZG9uIiwicHJlZmVycmVkX3VzZXJuYW1lIjoibWFya0B5YWxsYWNvb3BlcmF0aXZlLmNvbSIsImdpdmVuX25hbWUiOiJNYXJrIiwiZmFtaWx5X25hbWUiOiJDbGF5ZG9uIiwiZW1haWwiOiJtYXJrQHlhbGxhY29vcGVyYXRpdmUuY29tIn0.FgAiWAHWR-fI-wKWQTBWDVhFzkF4nL-up82r-HlwDfE8SUnUBOz1zFWAekThv7Xr27qu4QK0XG9i2AmiNpsnwxAqAllnKZflue9XQkP1X2EjLtKZqvFlsO6qiZfS1bQraDTlM_72w5XOX1EZl4LpVlE2FefFEsXuxUO-sbOrylZZxOLlkZQpXuOS4l97P_1H_SIWcAt0BppmnIcnoGGlsDgRXrG6wNaLmjIWrZBPUzp1DUlqd_LVyQTiLsJuOz7sJX7Ujp4Mngpetr1ieuW_DbLclyX1cF9FqbLGXGvjzI6pX14lc6PF8EFB05h8dBZxcmVuCKvIwh1fPlFeZEyioA',
-      refreshToken:
-        'eyJhbGciOiJIUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJmNDcyMmM0My0xNWI0LTRkN2EtYjg0MC00ZWQwYjU2ZmIzMmEifQ.eyJleHAiOjE3MzY4NzY5NDEsImlhdCI6MTcwNjcyMTAxMiwianRpIjoiZDM1ZjU2Y2QtYTNlNC00MDYyLTg1ZmYtNjIxN2ExMzgzOGNmIiwiaXNzIjoiaHR0cHM6Ly9sb2dpbi5sZXNjb21tdW5zLm9yZy9hdXRoL3JlYWxtcy9kYXRhLWZvb2QtY29uc29ydGl1bSIsImF1ZCI6Imh0dHBzOi8vbG9naW4ubGVzY29tbXVucy5vcmcvYXV0aC9yZWFsbXMvZGF0YS1mb29kLWNvbnNvcnRpdW0iLCJzdWIiOiJlNWI0MWRhNC0yNjMzLTQxMmYtODIxMC0xNTY5YTJkMGNlMjAiLCJ0eXAiOiJSZWZyZXNoIiwiYXpwIjoieWFsbGFkZXYiLCJzZXNzaW9uX3N0YXRlIjoiZDRhMTY3OTYtMjU4OC00OTY1LTgyNWMtMDFlOTQ0NTEzMGRkIiwic2NvcGUiOiJvcGVuaWQgcHJvZmlsZSBlbWFpbCIsInNpZCI6ImQ0YTE2Nzk2LTI1ODgtNDk2NS04MjVjLTAxZTk0NDUxMzBkZCJ9.cKozujhBSfz_oDI7Ks9EN634OiJgWFtFnrW5xS3VGEc',
-      idToken:
-        'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJKVjg1bVRtUmh1MGtSeGNNb0FGUFl5azJMbS1WTExYV25HOG9HbUxNUkowIn0.eyJleHAiOjE3MDY3MjI4MTIsImlhdCI6MTcwNjcyMTAxMiwiYXV0aF90aW1lIjoxNzA1MzQwOTQxLCJqdGkiOiJjNjk5YjkxZS01OTgwLTQ0Y2YtYWYyMy0yMzZkNzAwMTc1YzIiLCJpc3MiOiJodHRwczovL2xvZ2luLmxlc2NvbW11bnMub3JnL2F1dGgvcmVhbG1zL2RhdGEtZm9vZC1jb25zb3J0aXVtIiwiYXVkIjoieWFsbGFkZXYiLCJzdWIiOiJlNWI0MWRhNC0yNjMzLTQxMmYtODIxMC0xNTY5YTJkMGNlMjAiLCJ0eXAiOiJJRCIsImF6cCI6InlhbGxhZGV2Iiwic2Vzc2lvbl9zdGF0ZSI6ImQ0YTE2Nzk2LTI1ODgtNDk2NS04MjVjLTAxZTk0NDUxMzBkZCIsImF0X2hhc2giOiJyM1IzWFByNzVlU005NGxtbDA5VkN3IiwiYWNyIjoiMCIsInNpZCI6ImQ0YTE2Nzk2LTI1ODgtNDk2NS04MjVjLTAxZTk0NDUxMzBkZCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwibmFtZSI6Ik1hcmsgQ2xheWRvbiIsInByZWZlcnJlZF91c2VybmFtZSI6Im1hcmtAeWFsbGFjb29wZXJhdGl2ZS5jb20iLCJnaXZlbl9uYW1lIjoiTWFyayIsImZhbWlseV9uYW1lIjoiQ2xheWRvbiIsImVtYWlsIjoibWFya0B5YWxsYWNvb3BlcmF0aXZlLmNvbSJ9.Ha_r7QpSMvVEMV--2U3Ql3RoYwmvkt_JcMT19z_l-WOxmzk2rc9_SgUQHzxvcwuzvPP8CRNk7E5oXcJww7euqwm26suTG58OOVRQr4SJTJt9X28aVtjMxAB7XSNskaVDsnC0bkCXfSAnF439fhHE8Fu-lI3gYoqTPM2hPCQktcTkVZyPsfATTcTjXiKnUuCugX1YtPBBCAUIV-xjt5D-ewevh802JKp2fpkASihYKbsh-__wGvPhqBtNZ5nDjqW9EK79_oyCMpOhSs4NIRxcv3b89D9ZNzVIm_eOQbpcLDrV2Xe0ruKwqpu20sNLtfpnbgrHPce-mYhdimZMeqGbng'
-    };
-    // if (!req.user) {
-    //   return res.status(401).json({
-    //     success: false,
-    //     message: 'User not authenticated',
-    //     isAuthenticated: false
-    //   });
-    // }
+    if (!req.user) {
+      return res.status(401).json({
+        success: false,
+        message: 'User not authenticated',
+        isAuthenticated: false
+      });
+    }
 
-    // const { refreshToken } = req.user;
+    const { refreshToken } = req.user;
 
-    // const issuer = await Issuer.discover(issuerURL);
+    const issuer = await Issuer.discover(issuerURL);
 
-    // const client = new issuer.Client({
-    //   client_id: clientId,
-    //   client_secret: clientSecret
-    // });
+    const client = new issuer.Client({
+      client_id: clientId,
+      client_secret: clientSecret
+    });
 
-    // const tokenSet = await client.refresh(refreshToken);
+    const tokenSet = await client.refresh(refreshToken);
 
-    // const accessTokenSet = await client.introspect(tokenSet.access_token);
+    const accessTokenSet = await client.introspect(tokenSet.access_token);
 
-    // req.user.accessToken = tokenSet.access_token;
+    req.user.accessToken = tokenSet.access_token;
 
-    // if (!accessTokenSet.active) {
-    //   return res.status(403).json({
-    //     success: false,
-    //     message: 'User not authenticated',
-    //     isAuthenticated: false
-    //   });
-    // }
+    if (!accessTokenSet.active) {
+      return res.status(403).json({
+        success: false,
+        message: 'User not authenticated',
+        isAuthenticated: false
+      });
+    }
 
     return next();
   } catch (err) {
