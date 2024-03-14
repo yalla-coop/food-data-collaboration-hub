eee8aed27b5c8f605e822ab9370373af83ef26bc
Alex Luker <alex.luker@gmail.com>
2024-03-14 14:35:29 +0000

    Set variant to continue selling if inventory received as -1

--- web/connector/productUtils.test.js
+++ web/connector/productUtils.test.js
@@ -7,9 +7,20 @@ const { generateShopifyFDCProducts } = require('./productUtils');
 describe('generateShopifyFDCProducts', () => {
   it('should generate shopify FDC products from dfc exported JSON-LD', async () => {
     const result = await generateShopifyFDCProducts(
-      exportedDFCProducerProducts
+      exportedDFCProducerProducts()
     );
     expect(result).toBeInstanceOf(Array);
     expect(result).toEqual(importedShopifyProductsFromDFC);
   }, 15000);
+
+  it('variant inventory management set to continue when stock limitation -1', async () => {
+    const result = await generateShopifyFDCProducts(
+      exportedDFCProducerProducts(-1)
+    );
+
+    const variant = result[0].variants[0]
+
+    expect(variant.inventory_quantity).toEqual(-1);
+    expect(variant.inventory_policy).toEqual('continue');
+  });
 });
