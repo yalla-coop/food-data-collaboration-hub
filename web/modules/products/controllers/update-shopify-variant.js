138cf667347b4ea2f74bbee619b3f1c178cf7b5b
Simon Dupree <simon@yallacooperative.com>
2024-07-16 16:31:01 +0200

    refacotr update-shopify-variants

--- web/modules/products/controllers/update-shopify-variant.js
+++ web/modules/products/controllers/update-shopify-variant.js
@@ -3,30 +3,83 @@
 import shopify from '../../../shopify.js';
 import { query } from '../../../database/connect.js';
 
+const updateVariantMutation = `
+  mutation UpdateVariant($input: ProductVariantInput!) {
+    productVariantUpdate(input: $input) {
+      productVariant {
+        id
+        price
+      }
+      userErrors {
+        field
+        message
+      }
+    }
+  }
+`;
+
 const updateShopifyVariant = async (req, res) => {
   try {
     const { session } = res.locals.shopify;
+    const gqlClient = new shopify.api.clients.Graphql({ session });
+
     const { variantsMappingData } = req.body;
+    const { variantId } = req.params;
 
-    const variantId = req.params.variantId
-
-    const variantExists = await query('SELECT * FROM variants WHERE hub_variant_id = $1;', [variantId]);
+    const variantExists = await query(
+      'SELECT * FROM variants WHERE hub_variant_id = $1;',
+      [variantId]
+    );
 
     if (variantExists.rows.length !== 1) {
       throw new Error(`Variant ${variantId} doesn't exist`);
     }
 
-    const existingVariant = new shopify.api.rest.Variant({
-      session
-    });
-    existingVariant.id = variantId;
-    existingVariant.price =  variantsMappingData.price,
-    await existingVariant.saveAndUpdate();
+    const variantInput = {
+      id: variantId,
+      price: variantsMappingData.price
+    };
 
-    await query('UPDATE variants SET price = $1, added_value = $2, added_value_method = $3 WHERE hub_variant_id = $4', [variantsMappingData.price, variantsMappingData.addedValue, variantsMappingData.addedValueMethod, variantId]);
+    const response = await gqlClient.query({
+      data: {
+        query: updateVariantMutation,
+        variables: { input: variantInput }
+      }
+    });
+
+    const { data, errors } = response;
+
+    if (errors) {
+      console.error('GraphQL errors:', errors);
+      return res.status(500).json({
+        success: false,
+        errors
+      });
+    }
+
+    const { productVariantUpdate: { productVariant, userErrors } = {} } =
+      data || {};
+
+    if (userErrors && userErrors.length > 0) {
+      const errorMessage = userErrors
+        .map((err) => `${err.field}: ${err.message}`)
+        .join(', ');
+      throw new Error(`GraphQL user errors: ${errorMessage}`);
+    }
+
+    await query(
+      'UPDATE variants SET price = $1, added_value = $2, added_value_method = $3 WHERE hub_variant_id = $4',
+      [
+        variantsMappingData.price,
+        variantsMappingData.addedValue,
+        variantsMappingData.addedValueMethod,
+        variantId
+      ]
+    );
 
     return res.json({
-      success: true
+      success: true,
+      variant: productVariant
     });
   } catch (error) {
     console.error('Could not update Shopify product', error);
