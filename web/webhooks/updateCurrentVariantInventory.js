c71c30ae823bb2936fb9db6c7a45bec48b8a03e9
Simon Dupree <simon@yallacooperative.com>
2024-07-26 18:05:45 +0200

    refactor updateCurrentVariantInventory

--- web/webhooks/updateCurrentVariantInventory.js
+++ web/webhooks/updateCurrentVariantInventory.js
@@ -2,11 +2,80 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 import shopify from '../shopify.js';
 import { query } from '../database/connect.js';
-import { convertShopifyGraphQLIdToNumber, throwError } from '../utils/index.js';
+import { executeGraphQLQuery, throwError } from '../utils/index.js';
 import { generateShopifyFDCProducts } from '../connector/productUtils.js';
 
 dotenv.config();
 const { PRODUCER_SHOP_URL, PRODUCER_SHOP, HUB_SHOP_NAME } = process.env;
+
+const UPDATE_VARIANT_PRICE_MUTATION = `
+  mutation updateVariantPrice($id: ID!, $price: String!) {
+    variantUpdate(input: { id: $id, price: $price }) {
+      variant {
+        id
+        price
+      }
+      userErrors {
+        field
+        message
+      }
+    }
+  }
+`;
+
+const UPDATE_INVENTORY_MUTATION = `
+  mutation updateInventory($inventoryItemId: ID!, $available: Int!, $locationId: ID!) {
+    inventoryAdjustQuantity(input: {
+      inventoryItemId: $inventoryItemId,
+      availableDelta: $available,
+      locationId: $locationId
+    }) {
+      inventoryLevel {
+        id
+        available
+      }
+      userErrors {
+        field
+        message
+      }
+    }
+  }
+`;
+
+const UPDATE_VARIANT_DETAILS_MUTATION = `
+  mutation updateVariantDetails($id: ID!, $inventoryPolicy: String, $inventoryManagement: String) {
+    variantUpdate(input: {
+      id: $id,
+      inventoryPolicy: $inventoryPolicy,
+      inventoryManagement: $inventoryManagement
+    }) {
+      variant {
+        id
+        inventoryPolicy
+        inventoryManagement
+      }
+      userErrors {
+        field
+        message
+      }
+    }
+  }
+`;
+
+const GET_LOCATION_QUERY = `
+  query {
+    locations(first: 1) {
+      edges {
+        node {
+          id
+        }
+      }
+    }
+  }
+`;
+
+const updateVariantDBQuery =
+  'UPDATE variants SET price = $1 WHERE hub_variant_id = $2';
 
 export const calculateThePrice = ({
   originalPrice,
@@ -53,11 +122,26 @@ const getLatestProducerProductData = async (producerProductId, accessToken) => {
   }
 };
 
+const updateCurrentVariantPriceDB = async ({
+  hubVariantId,
+  hubVariantNewPrice
+}) => {
+  try {
+    const result = await query(updateVariantDBQuery, [
+      hubVariantNewPrice,
+      hubVariantId
+    ]);
+    return result.rows[0];
+  } catch (err) {
+    throwError('Error from updateCurrentVariantPrice db query', err);
+  }
+};
+
 const updateCurrentVariantPrice = async ({
   hubVariantId,
-  session,
   storedHubVariant,
-  mappedVariantPrice
+  mappedVariantPrice,
+  gqlClient
 }) => {
   try {
     const hubVariantNewPrice = calculateThePrice({
@@ -65,26 +149,65 @@ const updateCurrentVariantPrice = async ({
       _addingPriceType: storedHubVariant.addedValueMethod,
       markUpValue: storedHubVariant.addedValue,
       noOfItemsPerPackage: storedHubVariant.noOfItemsPerPackage
-    });
+    }).toFixed(2);
 
-    const existingVariant = new shopify.api.rest.Variant({
-      session
-    });
-    existingVariant.id = hubVariantId;
-    existingVariant.price = hubVariantNewPrice.toFixed(2);
-    // we should update also the existing price of this variant
-    await existingVariant.saveAndUpdate();
-    const updateVariantQuery =
-      'UPDATE variants SET price = $1 WHERE hub_variant_id = $2';
+    const variables = {
+      id: `gid://shopify/ProductVariant/${hubVariantId}`,
+      price: hubVariantNewPrice
+    };
 
-    try {
-      await query(updateVariantQuery, [hubVariantNewPrice, hubVariantId]);
-    } catch (err) {
-      throwError('Error from updateCurrentVariantPrice db query', err);
+    const mutationResponse = await executeGraphQLQuery(
+      gqlClient,
+      UPDATE_VARIANT_PRICE_MUTATION,
+      variables
+    );
+    if (mutationResponse.variantUpdate.userErrors.length > 0) {
+      throw new Error(
+        JSON.stringify(mutationResponse.variantUpdate.userErrors)
+      );
     }
+
+    return await updateCurrentVariantPriceDB({
+      hubVariantId,
+      hubVariantNewPrice
+    });
   } catch (err) {
     throwError('Error from updateCurrentVariantPrice', err);
   }
+};
+
+const getWholesaleProducerProduct = async ({
+  producerProductId,
+  producerProductData,
+  mappedVariantId,
+  hubVariantId,
+  accessToken
+}) => {
+  let wholesaleProducerProduct;
+
+  if (!producerProductData) {
+    wholesaleProducerProduct = (
+      await getLatestProducerProductData(producerProductId, accessToken)
+    )?.wholesaleProduct;
+  } else {
+    wholesaleProducerProduct = producerProductData.wholesaleProduct;
+  }
+
+  if (!wholesaleProducerProduct) {
+    console.error(
+      `Unable to load latest producer data for ${producerProductId}`
+    );
+    return null;
+  }
+
+  if (Number(wholesaleProducerProduct.id) !== Number(mappedVariantId)) {
+    console.error(
+      `Couldn't update the inventory for hub product ${hubVariantId}. The mapped wholesale variant was different, ${mappedVariantId} in hub, ${wholesaleProducerProduct.id} from producer`
+    );
+    return null;
+  }
+
+  return wholesaleProducerProduct;
 };
 
 export const updateCurrentVariantInventory = async ({
@@ -102,7 +225,6 @@ export const updateCurrentVariantInventory = async ({
       numberOfExcessOrders
     } = storedHubVariant;
     const sessionId = shopify.api.session.getOfflineId(HUB_SHOP_NAME);
-
     const session = await shopify.config.sessionStorage.loadSession(sessionId);
 
     if (!session) {
@@ -110,73 +232,78 @@ export const updateCurrentVariantInventory = async ({
         'Error from updateCurrentVariantInventory: Shopify Session not found'
       );
     }
+    const gqlClient = new shopify.api.clients.Graphql({ session });
 
-    let wholesaleProducerProduct;
-
-    if (!producerProductData) {
-      wholesaleProducerProduct = (
-        await getLatestProducerProductData(producerProductId, accessToken)
-      )?.wholesaleProduct;
-    } else {
-      wholesaleProducerProduct = producerProductData.wholesaleProduct;
-    }
-
-    if (!wholesaleProducerProduct) {
-      console.error(
-        `Unable to load latest producer data for ${producerProductId}`
-      );
-      return;
-    }
-
-    if (Number(wholesaleProducerProduct.id) !== Number(mappedVariantId)) {
-      console.error(
-        `Couldn't update the inventory for hub product ${hubVariantId}. The mapped wholesale variant was different, ${mappedVariantId} in hub, ${wholesaleProducerProduct.id} from producer`
-      );
-      return;
-    }
+    const wholesaleProducerProduct = await getWholesaleProducerProduct({
+      producerProductId,
+      producerProductData,
+      mappedVariantId,
+      hubVariantId,
+      accessToken
+    });
 
     if (shouldUpdateThePrice) {
       await updateCurrentVariantPrice({
         hubVariantId,
         session,
         storedHubVariant,
-        mappedVariantPrice: wholesaleProducerProduct.price
+        mappedVariantPrice: wholesaleProducerProduct.price,
+        gqlClient
       });
     }
-    const currentHubVariant = new shopify.api.rest.Variant({
-      session
-    });
 
-    currentHubVariant.id = hubVariantId;
+    const variablesForVariantUpdate = {
+      id: `gid://shopify/ProductVariant/${hubVariantId}`,
+      inventoryPolicy: wholesaleProducerProduct.inventoryPolicy,
+      inventoryManagement:
+        wholesaleProducerProduct.inventoryManagement || 'shopify'
+    };
 
-    currentHubVariant.inventoryPolicy =
-      wholesaleProducerProduct.inventoryPolicy;
-    currentHubVariant.inventoryManagement =
-      wholesaleProducerProduct.inventoryManagement || 'shopify';
-    await currentHubVariant.saveAndUpdate();
+    const locationQueryResponse = await executeGraphQLQuery(
+      gqlClient,
+      GET_LOCATION_QUERY
+    );
+    const locationId = locationQueryResponse.locations.edges[0].node.id;
 
-    const inventoryItemId = currentHubVariant.inventory_item_id;
+    const variantUpdatesMutationResponse = await executeGraphQLQuery(
+      gqlClient,
+      UPDATE_VARIANT_DETAILS_MUTATION,
+      variablesForVariantUpdate
+    );
 
-    const inventoryLevels = await shopify.api.rest.InventoryLevel.all({
-      session,
-      inventory_item_ids: inventoryItemId
-    });
+    if (variantUpdatesMutationResponse.variantUpdate.userErrors.length > 0) {
+      throw new Error(
+        JSON.stringify(variantUpdatesMutationResponse.variantUpdate.userErrors)
+      );
+    }
 
-    const inventoryLevel = new shopify.api.rest.InventoryLevel({
-      session
-    });
+    const inventoryItemId = `gid://shopify/InventoryItem/${hubVariantId}`;
 
     const availableItemsInTheStore =
       noOfItemsPerPackage * Number(wholesaleProducerProduct.inventoryQuantity) +
       Number(numberOfExcessOrders);
 
-    await inventoryLevel.set({
-      inventory_item_id: inventoryItemId,
+    const variablesForInventoryUpdate = {
+      inventoryItemId,
       available: availableItemsInTheStore || 0,
-      location_id: inventoryLevels.find(
-        (l) => l.inventory_item_id === inventoryItemId
-      ).location_id
-    });
+      locationId
+    };
+
+    const inventoryMutationResponse = await executeGraphQLQuery(
+      gqlClient,
+      UPDATE_INVENTORY_MUTATION,
+      variablesForInventoryUpdate
+    );
+
+    if (
+      inventoryMutationResponse.inventoryAdjustQuantity.userErrors.length > 0
+    ) {
+      throw new Error(
+        JSON.stringify(
+          inventoryMutationResponse.inventoryAdjustQuantity.userErrors
+        )
+      );
+    }
   } catch (err) {
     throwError('Error updating current variant inventory', err);
   }
